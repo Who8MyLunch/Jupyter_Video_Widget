5740315457662bccf2805365ba3cdefea79964e9
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2017-03-11 09:38:59 -0800

    work

--- js/src/video.js
+++ js/src/video.js
@@ -2,8 +2,8 @@ var widgets = require('jupyter-js-widgets');
 var _ = require('underscore');
 
 
-// Custom Model. Custom widgets models must at least provide default values
-// for model attributes when different from the base class.  These include `_model_name`,
+// Custom Model. Custom widgets models must at least provide default values for model
+// attributes when different from the base class.  These include `_model_name`,
 // `_view_name`, `_model_module`, and `_view_module` .
 //
 // When serialiazing entire widget state for embedding, only values different from the
@@ -16,7 +16,11 @@ var VideoModel = widgets.DOMWidgetModel.extend({
         _view_module: 'jupyter_video_widget',
         _model_module_version : '0.0.1',
         _view_module_version : '0.0.1',
-        url: 'Hello World'
+        src: '',
+        currentTime: 0.0,
+        _method: [],
+        _property: [],
+        _event: {},
     })
 });
 
@@ -38,15 +42,10 @@ var VideoModel = widgets.DOMWidgetModel.extend({
 
 // Media buffering and seeking, nice example displaying time ranges where video is loaded
 // https://developer.mozilla.org/en-US/Apps/Fundamentals/Audio_and_video_delivery/buffering_seeking_time_ranges
-
-// Rate playback explained
-// https://developer.mozilla.org/en-US/Apps/Fundamentals/Audio_and_video_delivery/WebAudio_playbackRate_explained
 var VideoView = widgets.DOMWidgetView.extend({
 
     render: function() {
         // This project's view is quite simple: just a single <video/> element.
-        console.log('render');
-
         this.video = document.createElement('video');
         this.setElement(this.video);
 
@@ -54,29 +53,18 @@ var VideoView = widgets.DOMWidgetView.extend({
         this.video.preload = 'auto';
         this.video.autoplay = false;
 
-        // this.video.addEventListener('loadedmetadata', function (e) {
-        //     var width = this.videoWidth;
-        //     var height = this.videoHeight;
-        //     console.log(this);
-        //     console.log(e);
-        //     console.log(width, height);
-        // }, false);
-
-        this.url_changed();
-        this.model.on('change:url', this.url_changed, this);
+        this.src_changed();
+        this.model.on('change:src', this.src_changed, this);
+        this.model.on('change:currentTime', this.currentTime_changed, this);
         this.model.on('change:_method', this.invoke_method, this);
         this.model.on('change:_property', this.set_property, this);
     },
 
-    url_changed: function() {
-        console.log('url_changed: ' + this.model.get('url'));
-        this.video.src = this.model.get('url');
-    },
-
     invoke_method: function() {
-        // Invoke method on video element.
+        // Invoke method on video element
+        // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
         var parts = this.model.get('_method');
-        console.log('parts: ', parts);
+
         var name = parts[0];
         var args = parts[1];
 
@@ -84,65 +72,70 @@ var VideoView = widgets.DOMWidgetView.extend({
     },
 
     set_property: function() {
-        // Set property value
+        // Set property value on video element
+        // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
         var parts = this.model.get('_property');
-        console.log('parts: ', parts);
+
         var name = parts[0];
         var value = parts[1];
 
         this.video[name] = value;
     },
 
-    // Video events
-    // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
-    // metadata: progress, loadeddata, ratechange, stalled, canplaythrough
+    src_changed: function() {
+        var field = 'src';
+        this.video[field] = this.model.get(field);
+    },
+
+    currentTime_changed: function() {
+        var field = 'currentTime';
+        this.video[field] = this.model.get(field);
+    },
 
     // Event handlers
+    // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
     events: {
+        // State information
+        durationchange: 'handle_event',
         ended: 'handle_event',
+        loadedmetadata: 'handle_event',
         pause: 'handle_event',
         play: 'handle_event',
         playing: 'handle_event',
+        ratechange: 'handle_event',
         seeked: 'handle_event',
-        timeupdate: 'handle_event',
+        seeking: 'handle_event',
         volumechange: 'handle_event',
 
-        canplaythrough: 'handle_event',
-        loadedmetadata: 'handle_event',
-        progress: 'handle_event',
-        ratechange: 'handle_event',
-        stalled: 'handle_event',
+        // Timeline information
+        timeupdate: 'handle_currentTime',
     },
 
     handle_event: function(ev) {
-        console.log(ev.type);
-        console.log(ev);
+        // Generic event handler
+        if (this.model.get('_handlers_active')) {
+            // Gather information
+            // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+            var fields = ['autoplay', 'clientHeight', 'clientWidth', 'controls',
+                          'duration', 'ended', 'loop', 'muted', 'paused', 'playbackRate',
+                          'seeking', 'videoHeight', 'videoWidth', 'volume']
 
-        // Build simple structure to send back to Python backend
-        var pev = {type: ev.type}
+            var pev = {'type': ev.type,
+                       'timeStamp': ev.timeStamp};
 
-        // event-specific information
-        if (ev.type == 'loadeddata') {
-            // Video info
-        } else if (ev.type == 'progress') {
-            // Timing info
+            for (let f of fields) {
+                pev[f] = ev.target[f];
+            }
 
+            this.model.set('_event', pev)
+            this.touch();  // Must call after any modifications to Model data.
         }
+    },
 
-
-        this.model.set('_property', ev)
-        this.touch(); // Must call after any modifications to Backbone Model data.
-
-
-    }
-
-    // handle_playing: function(ev) {
-    //     console.log(ev);
-    // }
-    // handle_pause: function(ev) {
-    //     console.log(ev);
-    // }
-
+    handle_currentTime: function(ev) {
+        this.model.set('currentTime', ev.target.currentTime);
+        this.touch();
+    },
 
 });
 
--- jupyter_video_widget/video.py
+++ jupyter_video_widget/video.py
@@ -1,7 +1,6 @@
 import ipywidgets as widgets
 import traitlets
 
-
 @widgets.register()
 class Video(widgets.DOMWidget):
     """
@@ -15,27 +14,50 @@ class Video(widgets.DOMWidget):
     _model_module = traitlets.Unicode('jupyter_video_widget').tag(sync=True)
     _model_module_version = traitlets.Unicode('0.0.1').tag(sync=True)
 
-    url = traitlets.Unicode().tag(sync=True)
+    # Behind-the-scenes information
     _method = traitlets.List().tag(sync=True)
     _property = traitlets.List().tag(sync=True)
-    _event = traitlets.List().tag(sync=True)
+    _event = traitlets.Dict().tag(sync=True)
+    # _handlers_active = traitlets.Bool(False).tag(sync=True)
 
-    def __init__(self, url=None):
+    # Public information
+    src = traitlets.Unicode().tag(sync=True)
+    currentTime = traitlets.Float().tag(sync=True)
+
+    def __init__(self, src=''):
         """
         Create new widget instance
         """
         super().__init__()
 
-        if url:
-            self.url = url
+        self.src = src
 
-    def _invoke_method(self, name, *args):
+        # Allow user-defined Python callback functions for any of these event types
+        # kinds = ['durationchange', 'ended', 'loadedmetadata', 'pause', 'play', 'playing',
+        #          'ratechange', 'seeked', 'volumechange']
+        # self._event_dispatchers = {}
+        # for k in kinds:
+        #     self._event_dispatchers[k] = widgets.widget.CallbackDispatcher()
+
+        # Manage user-defined Python event callback functions
+        self._event_dispatcher = widgets.widget.CallbackDispatcher()
+
+        self._state = {}
+
+    @property
+    def state(self):
+        """
+        Video widget state information
+        """
+        return self._state
+
+    def invoke_method(self, name, *args):
         """
         Invoke front-end method
         """
         self._method = [name, args]
 
-    def _set_property(self, name, value):
+    def set_property(self, name, value):
         """
         Assign value to front-end property
         """
@@ -44,18 +66,69 @@ class Video(widgets.DOMWidget):
     @traitlets.observe('_event')
     def _handle_event(self, change):
         """
-        Respond to front-end event
+        Respond to front-end events
         https://traitlets.readthedocs.io/en/stable/api.html#callbacks-when-trait-attributes-change
         """
-        print(change['type'], change['name'])
+        assert(change['type'] == 'change')
+        assert(change['name'] == '_event')
+        event = change['new']
+
+        # Update self state information
+        self._state = event
+
+        # Call any registered event handlers
+        self._event_dispatcher(self, event)
+        # try:
+        #     # Do it
+        #     kind = event['type']
+        #     self._event_dispatchers[kind](self, event)
+        # except KeyError:
+        #     raise ValueError('Unexpected kind of event: {}'.format(kind))
 
     #--------------------------------------------
+    # Register Python event handlers
+    # def _num_handlers(self):
+    #     numbers = [len(d.callbacks) for d in self._event_dispatchers.values()]
+    #     return sum(numbers)
 
+    def on_event(self, callback, remove=False):
+        """
+        Register frontend-event callback function.
+
+        Set keyword remove=True to unregister an existing callback function.
+
+        Supplied callback function(s) must accept two arguments: widget instance and event dict.
+        """
+        # Register/un-register
+        self._event_dispatcher.register_callback(callback, remove=remove)
+
+        # # Default to all defined event types
+        # if not kinds:
+        #     kinds = self._event_dispatchers.keys()
+        # if isinstance(kinds, str):
+        #     kinds = [kinds]
+        # for k in kinds:
+        #     if k not in self._event_dispatchers:
+        #         raise ValueError('Unexpected kind of callback: {}'.format(k))
+        #     # Register/un-register
+        #     self._event_dispatchers[k].register_callback(callback, remove=remove)
+        # Enable/disable event handling at front end.
+        # self._handlers_active = self._num_handlers() > 0
+
+    #--------------------------------------------
+    # Video methods
     def play(self):
-        self._invoke_method('play')
+        self.invoke_method('play')
 
     def pause(self):
-        self._invoke_method('pause')
+        self.invoke_method('pause')
 
+    def rewind(self):
+        self.set_property('currentTime', 0)
 
+    def seek(self, time):
+        self.set_property('currentTime', time)
 
+#------------------------------------------------
+if __name__ == '__main__':
+    pass
